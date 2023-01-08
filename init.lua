dfec597a89b0c73416efb7b7f9f87b80ec4c8a63
jiangyinzuo <392711804@qq.com>
2023-01-08 17:45:15 +0800

    initial commit

---
+++ init.lua
@@ -0,0 +1,182 @@
+vim.scriptencoding = "utf-8"
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
+
+vim.wo.number = true
+-- 高亮所在行
+vim.wo.cursorline = true
+-- 显示左侧图标指示列
+vim.wo.signcolumn = "yes"
+-- 右侧参考线，超过表示代码太长了，考虑换行
+vim.wo.colorcolumn = "80"
+-- 命令行高为2，提供足够的显示空间
+vim.o.cmdheight = 2
+-- 当文件被外部程序修改时，自动加载
+vim.o.autoread = true
+vim.bo.autoread = true
+-- 禁止折行
+vim.wo.wrap = false
+-- 光标在行首尾时<Left><Right>可以跳到下一行
+vim.o.whichwrap = "<,>,[,]"
+-- 允许隐藏被修改过的buffer
+vim.o.hidden = true
+-- 鼠标支持
+vim.o.mouse = "a"
+-- 禁止创建备份文件
+vim.o.backup = false
+vim.o.writebackup = false
+vim.o.swapfile = false
+-- 自动补全不自动选中
+vim.g.completeopt = "menu,menuone,noselect,noinsert"
+-- 样式
+vim.o.background = "dark"
+vim.o.termguicolors = true
+vim.opt.termguicolors = true
+-- 补全增强
+vim.o.wildmenu = true
+-- Dont' pass messages to |ins-completin menu|
+vim.o.shortmess = vim.o.shortmess .. "c"
+-- 补全最多显示10行
+vim.o.pumheight = 10
+-- 使用增强状态栏插件后不再需要 vim 的模式提示
+vim.o.showmode = false
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- 新行对齐当前行
+vim.o.autoindent = true
+vim.bo.autoindent = true
+vim.o.smartindent = true
+vim.cmd [[
+augroup cpp
+    autocmd!
+    autocmd FileType cpp setlocal noexpandtab tabstop=2 shiftwidth=2 softtabstop=2
+augroup end
+augroup python
+    autocmd!
+    autocmd FileType python setlocal noexpandtab tabstop=4
+augroup end
+augroup lua
+    autocmd!
+    autocmd FileType lua setlocal noexpandtab tabstop=2 shiftwidth=2 softtabstop=2
+augroup end
+]]
+
+require("packer").startup(function()
+	use "wbthomason/packer.nvim" -- Package manager
+	use { "williamboman/mason.nvim" }
+
+	use "folke/tokyonight.nvim"
+	use { "catppuccin/nvim", as = "catppuccin" }
+
+	use { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" }
+	use { "nvim-tree/nvim-tree.lua", requires = "nvim-tree/nvim-web-devicons" }
+
+	use { "akinsho/bufferline.nvim", tag = "v3.*", requires = { "nvim-tree/nvim-web-devicons", "moll/vim-bbye" } }
+	use { "nvim-lualine/lualine.nvim", requires = { "kyazdani42/nvim-web-devicons" } }
+
+	use {
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.0",
+		requires = { { "nvim-lua/plenary.nvim" } },
+	}
+
+	use "neovim/nvim-lspconfig" -- Configurations for Nvim LSP
+	use "p00f/clangd_extensions.nvim"
+	-- 补全引擎
+	use "hrsh7th/nvim-cmp"
+	-- snippet 引擎
+	use "hrsh7th/vim-vsnip"
+	-- 补全源
+	use "hrsh7th/cmp-vsnip"
+	use "hrsh7th/cmp-nvim-lsp" -- { name = nvim_lsp }
+	use "hrsh7th/cmp-buffer" -- { name = 'buffer' },
+	use "hrsh7th/cmp-path" -- { name = 'path' }
+	use "hrsh7th/cmp-cmdline" -- { name = 'cmdline' }
+	-- 常见编程语言代码段
+	use "rafamadriz/friendly-snippets"
+
+	use "lukas-reineke/indent-blankline.nvim"
+	use { "jose-elias-alvarez/null-ls.nvim", requires = "nvim-lua/plenary.nvim" }
+
+	use { "akinsho/toggleterm.nvim", tag = '*', config = function()
+		require("toggleterm").setup()
+	end }
+
+	git = {
+		-- default_url_format = 'https://gitclone.com/github.com/%s'
+		-- default_url_format = 'https://hub.fastgit.xyz/%s'
+	}
+end)
+
+vim.cmd [[colorscheme catppuccin]]
+
+require("nvim-treesitter.configs").setup {
+	-- 安装 language parser
+	-- :TSInstallInfo 命令查看支持的语言
+	ensure_installed = { "cpp", "lua", "vim" },
+	-- 启用代码高亮模块
+	highlight = {
+		enable = true,
+		additional_vim_regex_highlighting = false,
+	},
+}
+
+-- disable netrw at the very start of your init.lua (strongly advised)
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
+
+-- empty setup using defaults
+require("nvim-tree").setup()
+
+require("bufferline").setup {
+	-- 侧边栏配置
+	-- 左侧让出 nvim-tree 的位置，显示文字 File Explorer
+	options = {
+		offsets = {
+			{
+				filetype = "NvimTree",
+				text = "File Explorer",
+				highlight = "Directory",
+				text_align = "left",
+			},
+		},
+	},
+}
+require("lualine").setup {}
+local telescope = require("telescope")
+telescope.setup {}
+
+-- Telescope
+-- 查找文件
+vim.keymap.set("n", "<C-p>", ":Telescope find_files<CR>", { noremap = true })
+-- 全局搜索
+vim.keymap.set("n", "<C-f>", ":Telescope live_grep<CR>", { noremap = true })
+
+require("mason").setup {
+	ui = {
+		icons = {
+			package_installed = "✓",
+			package_pending = "➜",
+			package_uninstalled = "✗",
+		},
+	},
+	github = {
+		-- The template URL to use when downloading assets from GitHub.
+		-- The placeholders are the following (in order):
+		-- 1. The repository (e.g. "rust-lang/rust-analyzer")
+		-- 2. The release version (e.g. "v0.3.0")
+		-- 3. The asset name (e.g. "rust-analyzer-v0.3.0-x86_64-unknown-linux-gnu.tar.gz")
+		download_url_template = "https://cors.isteed.cc/github.com/%s/releases/download/%s/%s",
+	},
+}
+
+require "lsp"
+vim.cmd [[autocmd Filetype c,cpp packadd termdebug]]
+vim.g.termdebug_wide = 1
+
+vim.keymap.set("n", "<leader>git", "<cmd>lua require('terminal').git_client_toggle()<CR>", { noremap = true })
---
+++ lua/lsp.lua
@@ -0,0 +1,164 @@
+vim.diagnostic.config {
+	virtual_text = true,
+	signs = true,
+}
+
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+	-- Enable completion triggered by <c-x><c-o>
+	vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+
+	-- Mappings.
+	-- See `:help vim.lsp.*` for documentation on any of the below functions
+	local bufopts = { noremap = true, silent = true, buffer = bufnr }
+	vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
+	vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+	vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+	vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
+	vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
+	vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, bufopts)
+	vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
+	vim.keymap.set("n", "<space>wl", function()
+		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+	end, bufopts)
+	vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, bufopts)
+	vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, bufopts)
+	vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, bufopts)
+	vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
+	vim.keymap.set("n", "<space>f", function()
+		vim.lsp.buf.format { async = true }
+	end, bufopts)
+end
+
+require("lspconfig").clangd.setup {
+	on_attach = on_attach,
+}
+
+require("clangd_extensions").setup {
+	server = {
+		-- options to pass to nvim-lspconfig
+		-- i.e. the arguments to require("lspconfig").clangd.setup({})
+		on_attach = on_attach
+	},
+	extensions = {
+		-- defaults:
+		-- Automatically set inlay hints (type hints)
+		autoSetHints = true,
+		-- These apply to the default ClangdSetInlayHints command
+		inlay_hints = {
+			-- Only show inlay hints for the current line
+			only_current_line = false,
+			-- Event which triggers a refersh of the inlay hints.
+			-- You can make this "CursorMoved" or "CursorMoved,CursorMovedI" but
+			-- not that this may cause  higher CPU usage.
+			-- This option is only respected when only_current_line and
+			-- autoSetHints both are true.
+			only_current_line_autocmd = "CursorHold",
+			-- whether to show parameter hints with the inlay hints or not
+			show_parameter_hints = true,
+			-- prefix for parameter hints
+			parameter_hints_prefix = "<- ",
+			-- prefix for all the other hints (type, chaining)
+			other_hints_prefix = "=> ",
+			-- whether to align to the length of the longest line in the file
+			max_len_align = false,
+			-- padding from the left if max_len_align is true
+			max_len_align_padding = 1,
+			-- whether to align to the extreme right or not
+			right_align = false,
+			-- padding from the right if right_align is true
+			right_align_padding = 7,
+			-- The color of the hints
+			highlight = "Comment",
+			-- The highlight group priority for extmark
+			priority = 100,
+		},
+		ast = {
+			-- These are unicode, should be available in any font
+			role_icons = {
+				type = "🄣",
+				declaration = "🄓",
+				expression = "🄔",
+				statement = ";",
+				specifier = "🄢",
+				["template argument"] = "🆃",
+			},
+			kind_icons = {
+				Compound = "🄲",
+				Recovery = "🅁",
+				TranslationUnit = "🅄",
+				PackExpansion = "🄿",
+				TemplateTypeParm = "🅃",
+				TemplateTemplateParm = "🅃",
+				TemplateParamObject = "🅃",
+			},
+			--[[ These require codicons (https://github.com/microsoft/vscode-codicons)
+				    role_icons = {
+                type = "",
+                declaration = "",
+                expression = "",
+                specifier = "",
+                statement = "",
+                ["template argument"] = "",
+            },
+
+            kind_icons = {
+                Compound = "",
+                Recovery = "",
+                TranslationUnit = "",
+                PackExpansion = "",
+                TemplateTypeParm = "",
+                TemplateTemplateParm = "",
+                TemplateParamObject = "",
+            }, ]]
+
+			highlights = {
+				detail = "Comment",
+			},
+		},
+		memory_usage = {
+			border = "none",
+		},
+		symbol_info = {
+			border = "none",
+		},
+	},
+}
+
+require("lspconfig").pyright.setup {
+	on_attach = on_attach,
+}
+require("lspconfig").sumneko_lua.setup {
+	settings = {
+		Lua = {
+			runtime = {
+				-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+				version = "LuaJIT",
+			},
+			diagnostics = {
+				-- Get the language server to recognize the `vim` global
+				globals = { "vim" },
+			},
+			workspace = {
+				-- Make the server aware of Neovim runtime files
+				library = vim.api.nvim_get_runtime_file("", true),
+			},
+			-- Do not send telemetry data containing a randomized but unique identifier
+			telemetry = {
+				enable = false,
+			},
+		},
+	},
+	on_attach = on_attach,
+}
+
+local null_ls = require "null-ls"
+
+null_ls.setup {
+	sources = {
+		null_ls.builtins.formatting.stylua,
+		null_ls.builtins.diagnostics.eslint,
+		null_ls.builtins.completion.spell,
+	}
+}
